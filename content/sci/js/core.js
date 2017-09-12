a4c8d464167246c1bcabd219c2ffaceceef44f45
Beck Johnson <rebeckah.j.johnson@avanade.com>
2017-09-12 09:48:12 -0700

    #9 Mobile global header nav dropdown  - made mobile nav show underneath header instead of sliding right

--- content/sci/js/core.js
+++ content/sci/js/core.js
@@ -1,4 +1,4 @@
-/*! 2017-09-08 18:01:53 */
+/*! 2017-09-11 18:33:33 */
 (function ($) {
 
 	var o = $({});
@@ -994,134 +994,6 @@
 
 (function ($) {
 
-	var FILTERED = 'gs-filtered';
-
-	bindEvents();
-
-	function bindEvents() {
-		$('body')
-			.on('click', '[data-global-selector]', onOpenTriggerClick)
-			.on('click', '#gs-mobile-filter-trigger', onMobileFilterClick)
-			.on('click', '#gs-mobile-filter-options > li > a, #gs-sidebar-items a', onRegionClick)
-			.on('input', '#gs-search-input', onSearchChanged)
-			.on('click', '.gs-country-title', onCountryClick)
-			.on('click', '#gs-close-wrapper', onCloseClick);
-	}
-
-	function onOpenTriggerClick(e) {
-		e.preventDefault();
-		var region = $(this).data('globalSelector');
-		if (region) {
-			setActiveRegion(region);
-		}
-		setGlobalSelectorVisibility(true);
-	}
-
-	function onMobileFilterClick() {
-		$('#gs-mobile-filter').toggleClass('gs-mobile-filter-open');
-	}
-
-	function onRegionClick(e) {
-		e.preventDefault();
-		setActiveRegion($(this).attr('rel'));
-	}
-
-	function onSearchChanged(e) {
-		var phrase = $(e.target).val();
-		applyCountryFilter(phrase);
-	}
-
-	function onCountryClick(e) {
-		$(this).parent().toggleClass('gs-country-open');
-	}
-
-	function onCloseClick() {
-		setGlobalSelectorVisibility(false);
-	}
-
-	function setActiveRegion(key) {
-		var region = $('#gs-sidebar-items a[rel=' + key + ']'),
-			name = region.text(),
-			wrapper = $('#' + key);
-		if (!region.length) return;
-		setActiveRegionWrapper(wrapper);
-		setMobileFilterText(name);
-		closeMobileFilter();
-		setSidebarSelection(key);
-		setSearchPlaceholder(name);
-	}
-
-	function applyCountryFilter(s) {
-		if (!s) {
-			clearCountryFilter();
-			return;
-		}
-
-		s = s.toLowerCase();
-		$('.gs-region').each(function () {
-			filterRegion($(this), s);
-		});
-	}
-
-	function filterRegion(region, s) {
-		var numFound = 0;
-		region.find('.gs-country').each(function () {
-			var country = $(this),
-				name = country.find('.gs-country-title').text(),
-				filtered = name.toLowerCase().indexOf(s) === -1;
-			filter(country, filtered);
-			if (!filtered) {
-				numFound++;
-			}
-		});
-
-		filter(region, numFound === 0);
-	}
-
-	function clearCountryFilter() {
-		filter($('.gs-region'), false);
-		filter($('.gs-country'), false);
-	}
-
-	function setActiveRegionWrapper(region) {
-		region.addClass('gs-region-wrapper-active').siblings().removeClass('gs-region-wrapper-active');
-		$('#gs-content-wrapper').scrollTop(0);
-	}
-
-	function setSidebarSelection(regionName) {
-		$('#gs-sidebar-items a[rel=' + regionName + ']').parent().addClass('gs-sidebar-item-active').siblings().removeClass('gs-sidebar-item-active');
-	}
-
-	function setSearchPlaceholder(regionName) {
-		var search = $('#gs-search-input');
-		search.attr('placeholder', search.data('placeholderInit') + ' ' + regionName);
-	}
-
-	function closeMobileFilter() {
-		$('#gs-mobile-filter').removeClass('gs-mobile-filter-open');
-	}
-
-	function setMobileFilterText(text) {
-		$('#gs-mobile-filter-trigger').text(text);
-	}
-
-	function setGlobalSelectorVisibility(on) {
-		var method = on ? 'addClass' : 'removeClass';
-		$('html')[method]('nav-global-selector-open');
-		$('#global-selector')[method]('gs-open');
-		if (on) {
-			$('body').scrollTop(0);
-		}
-	}
-
-	function filter(elements, filtered) {
-		elements[filtered ? 'addClass' : 'removeClass'](FILTERED);
-	}
-
-}(window.jQuery));;
-
-(function ($) {
-
 	bindEvents();
 
 	function bindEvents() {
@@ -1264,73 +1136,21 @@
 
 (function ($) {
 
-	var SPEED = 350, // this should match the CSS transition time
-		PANEL = 'nav-main-mobile-panel',
-		PANEL_ACTIVE = 'nav-main-mobile-panel-active',
-		PANEL_PARENT = 'nav-main-mobile-panel-parent',
-		PANEL_HIGHEST = 'nav-main-mobile-panel-highest';
+    var SPEED = 350; // this should match the CSS transition time
+		
+    $('.nav-main-list-trigger').on('click', function() {
+    
+	    var $html = $('html').toggleClass('nav-main-mobile-open').addClass('nav-main-mobile-transition');
+	    setTimeout((function () {
+	        $html.removeClass('nav-main-mobile-transition');
+	    }), SPEED);
+	});
 
-	bindEvents();
-
-	function bindEvents() {
-		$('body')
-			.on('click', '.nav-main-list-trigger', toggleMobileMenu)
-			.on('click', '.' + PANEL + ' > li > a', onMenuItemClick);
-	}
-
-	function toggleMobileMenu() {
-		var $html = $('html');
-		$html.toggleClass('nav-main-mobile-open');
-		$html.removeClass('nav-meganav-open');
-		setTransitionClass($html, 'nav-main-mobile-transition');
-	}
-
-	function onMenuItemClick(e) {
-		//var $link = $(this),
-		//	target = $link.attr('href'),
-		//	isMenuNav = (target[0] === '#');
-
-		//if (!isMenuNav) return;
-
-		//e.preventDefault();
-
-		//if (target === '#') {
-		//	toggleMobileMenu();
-		//	return;
-		//}
-
-		//var $thisPanel = $link.closest('.' + PANEL),
-		//	$targetPanel = $(target),
-		//	isBackNav = ($link.parent().index() === 0);
-
-		//setActivePanel($targetPanel);
-		//setTransitionClass(isBackNav ? $thisPanel : $targetPanel, PANEL_HIGHEST);
-	}
-
-	function setTransitionClass(element, className) {
-		element.addClass(className);
-		transitionDelay(function () {
-			element.removeClass(className);
-		});
-	}
-
-	function transitionDelay(fn) {
-		setTimeout(fn, SPEED);
-	}
-
-	function setActivePanel(panel) {
-		$('.' + PANEL_ACTIVE).removeClass(PANEL_ACTIVE);
-		$('.' + PANEL_PARENT).removeClass(PANEL_PARENT);
-		panel.addClass(PANEL_ACTIVE);
-		findParentPanel(panel).addClass(PANEL_PARENT);
-	}
-
-	function findParentPanel(childPanel) {
-		var firstLink = childPanel.find('li:first-child > a'),
-			parentSelector = firstLink.attr('href');
-		return $(parentSelector);
-	}
-
+    $('#nav-main-mobile').on('show.bs.collapse', function(e) {
+        $(e.target).parent('li').addClass('openPanel');
+    }).on('hidden.bs.collapse', function (e) {
+        $(e.target).parent('li').removeClass('openPanel');
+    });
 }(window.jQuery));;
 
 (function ($) {
