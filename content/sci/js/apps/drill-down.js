f2321aae89d943c520a343cd826c23bd27dc0858
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-09-13 17:42:12 -0700

    Added search box for variation 4

---
+++ content/sci/js/apps/drill-down.js
@@ -0,0 +1,137 @@
+/*! drill-down 2017-09-13 12:04:30 */
+angular.module('drill-down', ['xom.shared']);;
+
+angular.module('drill-down').controller('DrillDownCtrl', ['$scope', '$http', '$interpolate', 'eventBusSvc', 'DRILL_DOWN',
+	function ($scope, $http, $interpolate, eventBusSvc, DRILL_DOWN) {
+
+		'use strict';
+
+		$scope.breadcrumbs = [];
+		$scope.error = null;
+		$scope.list = null;
+		$scope.loading = false;
+		$scope.previousList = null;
+
+		$scope.goBack = goBack;
+		$scope.loadList = loadList;
+
+		$scope.$watch('loading', function (v) {
+			eventBusSvc.publish('xom.loading', v);
+		});
+
+		DRILL_DOWN.groupingRules.sort(sortGroupingRules);
+
+		if (DRILL_DOWN.searchOnLoad) {
+			loadList(DRILL_DOWN.initialApiUrl);
+		}
+
+		if (DRILL_DOWN.phraseEventName) {
+			eventBusSvc.subscribe(DRILL_DOWN.phraseEventName, onSearchRequested);
+		}
+
+		function loadList(url, skipBreadcrumb) {
+			if ($scope.loading) return;
+			$scope.loading = true;
+
+			if ($scope.list && !skipBreadcrumb) {
+				$scope.breadcrumbs.push(angular.copy($scope.list));
+			}
+
+			$http.get(url).then(onListReceived, onListError).finally(onListComplete);
+		}
+
+		function onListReceived(response) {
+			$scope.error = null;
+			$scope.list = response.data.conditionlist;
+			$scope.list.url = response.config.url;
+			buildGroups($scope.list);
+			setPreviousList();
+		}
+
+		function onListError(response) {
+			$scope.error = response;
+		}
+
+		function onListComplete() {
+			$scope.loading = false;
+		}
+
+		function onSearchRequested(e, args) {
+			var expression = $interpolate(DRILL_DOWN.initialApiUrl);
+			var url = expression(args);
+			$scope.breadcrumbs = [];
+			$scope.list = null;
+			$scope.previousList = null;
+			loadList(url);
+		}
+
+		function buildGroups(list) {
+			var itemCount = list.results.length;
+			var columnCount = getColumnCount(itemCount);
+			var itemsPerColumn = calculateItemsPerColumn(itemCount, columnCount);
+
+			list.groups = [];
+			for (var i = 0; i < list.results.length; i++) {
+				if (i % itemsPerColumn === 0) {
+					list.groups.push([]);
+				}
+				list.groups[list.groups.length - 1].push(list.results[i]);
+			}
+		}
+
+		function getColumnCount(itemCount) {
+			for (var i = 0; i < DRILL_DOWN.groupingRules.length; i++) {
+				if (DRILL_DOWN.groupingRules[i].minItems <= itemCount) {
+					return DRILL_DOWN.groupingRules[i].cols;
+				}
+			}
+			return 3;
+		}
+
+		function calculateItemsPerColumn(itemCount, columnCount) {
+			var itemsPerColumn = parseInt(itemCount / columnCount);
+			if (itemCount % columnCount !== 0) itemsPerColumn++;
+			return itemsPerColumn;
+		}
+
+		function sortGroupingRules(a, b) {
+			return -(a.minItems - b.minItems);
+		}
+
+		function goBack() {
+			if ($scope.breadcrumbs.length < 1) return;
+			var lastList = $scope.breadcrumbs.pop();
+			loadList(lastList.url, true);
+		}
+
+		function setPreviousList() {
+			$scope.previousList = ($scope.breadcrumbs.length < 1 ? null : $scope.breadcrumbs[$scope.breadcrumbs.length - 1]);
+		}
+	}
+]);;
+
+(function ($) {
+	$('script[data-app]').each(function () {
+		var $this = $(this),
+			appName= $this.data('app'),
+			constants = $.parseJSON($this.html()); // use html() instead of text() so it works on IE8
+		for (var key in constants) {
+			angular.module(appName).constant(key, constants[key]);
+		}
+	});
+}(window.jQuery));;
+
+angular.module('xom.shared', []);;
+
+(function ($) {
+
+	'use strict';
+
+	angular.module('xom.shared').factory('eventBusSvc', function () {
+		return {
+			publish: $.publish,
+			subscribe: $.subscribe
+		};
+	});
+
+}(window.jQuery));
--- content/sci/js/core.js
+++ content/sci/js/core.js
@@ -1,4 +1,4 @@
-/*! 2017-09-13 15:52:20 */
+/*! 2017-09-13 15:17:53 */
 (function ($) {
 
 	var o = $({});
@@ -578,286 +578,306 @@ $(document).ready(function () {
 
 (function ($) {
 
-	var COUNTRY_SETTING_KEY = 'country';
+    var COUNTRY_SETTING_KEY = 'country';
 
-	var typeaheadDefaults = {
-		highlight: true,
-		minLength: 3
-	};
+    var typeaheadDefaults = {
+        highlight: true,
+        minLength: 3
+    };
 
-	var userHasChangedCountry = false;
+    var userHasChangedCountry = false;
 
-	$(function () {
-		renderAllRecentSearches();
-		loadSearchSettings();
-		bindCoveoTypeahead();
-		bindSearchBox();
-		bindSearchSettingModals();
-	});
+    $(function () {
+        renderAllRecentSearches();
+        loadSearchSettings();
+        bindCoveoTypeahead();
+        bindSearchBox();
+        bindSearchSettingModals();
+    });
 
-	function renderAllRecentSearches() {
-		$('.search-box').each(function () {
-			renderRecentSearches($(this));
-		});
-	}
+    function renderAllRecentSearches() {
+        $('.search-box').each(function () {
+            renderRecentSearches($(this));
+        });
+    }
 
-	function bindCoveoTypeahead() {
-		$('.typeahead').each(function () {
-			var $this = $(this);
-			var options = $.extend({}, typeaheadDefaults, $this.data('options'));
-			var bloodhound = createCoveoBloodhound($this.data('url'), $this.data('wildcard') || '%QUERY');
-			bloodhound.initialize();
-			$this.typeahead(options, {
-				displayKey: 'title',
-				source: bloodhound.ttAdapter()
-			});
-		});
-	}
+    function bindCoveoTypeahead() {
+        $('.typeahead').each(function () {
+            var $this = $(this);
+            var options = $.extend({}, typeaheadDefaults, $this.data('options'));
+            var bloodhound = createCoveoBloodhound($this.data('url'), $this.data('wildcard') || '%QUERY');
+            bloodhound.initialize();
+            $this.typeahead(options, {
+                displayKey: 'title',
+                source: bloodhound.ttAdapter()
+            });
+        });
+    }
 
-	function bindSearchBox() {
-		var INPUT_SELECTOR = '.search-box input[type="text"]';
-		$('body')
-			.on('focus', INPUT_SELECTOR, function () {
-				onSearchInputFocus($(this));
-			})
-			.on('keydown', INPUT_SELECTOR, function (e) {
-				if (e.keyCode != 13) return;
-				e.preventDefault();
-				onSearchTriggered($(this));
-			})
-			.on('click', '.search-box button.search', function () {
-				onSearchTriggered($(this));
-			});
-		$.subscribe('xom.geolocation', onGeolocation);
-	}
+    function bindSearchBox() {
+        var INPUT_SELECTOR = '.search-box input[type="text"]';
+        $('body')
+            .on('focus', INPUT_SELECTOR, function () {
+                onSearchInputFocus($(this));
+            })
+            .on('keydown', INPUT_SELECTOR, function (e) {
+                if (e.keyCode != 13) return;
+                e.preventDefault();
+                onSearchTriggered($(this));
+            })
+            .on('click', '.search-box button.search', function () {
+                onSearchTriggered($(this));
+            });
+        $.subscribe('xom.geolocation', onGeolocation);
+    }
 
-	function bindSearchSettingModals() {
-		$('body')
-			.on('show.bs.modal', '.modal[data-search-key]', function (e) {
-				var modal = $(e.target);
-				onSearchSettingModalOpened(modal);
-			})
-			.on('click', '.modal[data-search-key]', function (e) {
-				var target = $(e.target);
-				var modal = target.closest('.modal');
-				if (target.is('label')) {
-					onSearchSettingClicked(modal);
-				} else if (target.is('.btn-primary')) {
-					onSearchSettingChanged(modal);
-				}
-			});
-	}
+    function bindSearchSettingModals() {
+        $('body')
+            .on('show.bs.modal', '.modal[data-search-key]', function (e) {
+                var modal = $(e.target);
+                onSearchSettingModalOpened(modal);
+            })
+            .on('click', '.modal[data-search-key]', function (e) {
+                var target = $(e.target);
+                var modal = target.closest('.modal');
+                if (target.is('label')) {
+                    onSearchSettingClicked(modal);
+                } else if (target.is('.btn-primary')) {
+                    onSearchSettingChanged(modal);
+                }
+            });
+    }
 
-	function createCoveoBloodhound(url, wildcard) {
-		return new Bloodhound({
-			datumTokenizer: function (datum) {
-				return Bloodhound.tokenizers.whitespace(datum.title);
-			},
-			queryTokenizer: Bloodhound.tokenizers.whitespace,
-			remote: {
-				url: url,
-				wildcard: wildcard,
-				filter: function (response) {
-					return response.results.map(function (r) {
-						return {
-							title: r.title,
-							url: r.clickUri
-						};
-					});
-				}
-			}
-		});
-	}
+    function createCoveoBloodhound(url, wildcard) {
+        return new Bloodhound({
+            datumTokenizer: function (datum) {
+                return Bloodhound.tokenizers.whitespace(datum.title);
+            },
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: url,
+                wildcard: wildcard,
+                filter: function (response) {
+                    return response.results.map(function (r) {
+                        return {
+                            title: r.title,
+                            url: r.clickUri
+                        };
+                    });
+                }
+            }
+        });
+    }
 
-	function loadSearchSettings() {
-		$('.search-box-with-settings').each(function () {
-			var $this = $(this);
-			var data = $this.data() || {};
-			if (!data.settingsCookie) return;
-			var settings = getSearchSettingsFromCookie(data.settingsCookie);
-			for (var k in settings) {
-				setSearchSetting(k, settings[k].value, settings[k].text);
-				if (k === COUNTRY_SETTING_KEY) $this.data('countryLoaded', true);
-			}
-		});
-	}
+    function loadSearchSettings() {
+        $('.search-box-with-settings').each(function () {
+            var $this = $(this);
+            var data = $this.data() || {};
+            if (!data.settingsCookie) return;
+            var settings = getSearchSettingsFromCookie(data.settingsCookie);
+            for (var k in settings) {
+                setSearchSetting(k, settings[k].value, settings[k].text);
+                if (k === COUNTRY_SETTING_KEY) $this.data('countryLoaded', true);
+            }
+        });
+    }
 
-	function onSearchInputFocus(input) {
-		var data = input.data('analytics');
-		if (!data) return;
-		$.publish('xom.analytics', data);
-	}
+    function onSearchInputFocus(input) {
+        var data = input.data('analytics');
+        if (!data) return;
+        $.publish('xom.analytics', data);
+    }
 
-	function onSearchTriggered(trigger) {
-		var searchBox = getSearchBoxElement(trigger);
-		var phrase = searchBox.find('input[type="text"]').not('.tt-hint').val();
-		if (!phrase) return;
+    function onSearchTriggered(trigger) {
+        var searchBox = getSearchBoxElement(trigger);
+        var phrase = searchBox.find('input[type="text"]').not('.tt-hint').val();
+        if (!phrase) return;
 
-		var data = searchBox.data() || {};
+        var data = searchBox.data() || {};
 
-		var query = {};
-		query[data.searchKey || 'q'] = phrase;
-		var settings = getSearchSettings(searchBox);
-		for (var k in settings) {
-			query[k] = settings[k].value;
-		}
+        var query = {};
+        query[data.searchKey || 'q'] = phrase;
+        var settings = getSearchSettings(searchBox);
+        for (var k in settings) {
+            query[k] = settings[k].value;
+        }
 
-		var analytics = trigger.data('analytics');
-		if (analytics) {
-			var defaultAnalytics = {
-				cg: 'Search',
-				csg: 'Search',
-				ct: 'Button',
-				oss: phrase
-			};
-			$.publish('xom.analytics', $.extend({}, defaultAnalytics, analytics));
-		}
+        var analytics = trigger.data('analytics');
+        if (analytics) {
+            var defaultAnalytics = {
+                cg: 'Search',
+                csg: 'Search',
+                ct: 'Button',
+                oss: phrase
+            };
+            $.publish('xom.analytics', $.extend({}, defaultAnalytics, analytics));
+        }
 
-		if (data.eventName) {
-			$.publish(data.eventName, query);
-		}
+        if (data.eventName) {
+            $.publish(data.eventName, query);
+        }
 
-		if (data.cookieName) {
-			var cookieUrl = $.fn.urlUtil.mergeQuery(data.url || window.location.href, query);
-			$.fn.siteSearch.add(data.cookieName, phrase, cookieUrl);
-		}
+        if (data.cookieName) {
+            var cookieUrl = $.fn.urlUtil.mergeQuery(data.url || window.location.href, query);
+            $.fn.siteSearch.add(data.cookieName, phrase, cookieUrl);
+        }
 
-		if (data.url) {
-			// A slight pause gives analytics time to fire
-			setTimeout(function () {
-				window.location = $.fn.urlUtil.mergeQuery(data.url, query);
-			}, 200);
-		} else if (data.cookieName) {
-			renderRecentSearches(searchBox);
-		}
-	}
+        if (data.url) {
+            // A slight pause gives analytics time to fire
+            setTimeout(function () {
+                window.location = $.fn.urlUtil.mergeQuery(data.url, query);
+            }, 200);
+        } else if (data.cookieName) {
+            renderRecentSearches(searchBox);
+        }
+    }
 
-	function onSearchSettingModalOpened(modal) {
-		var searchKey = modal.data('searchKey');
-		var trigger = $('.search-box-setting[data-key="' + searchKey + '"]');
-		var values = trigger.data('value').split(',');
-		modal.find('input').each(function () {
-			var input = $(this);
-			var selected = values.indexOf(input.val()) !== -1;
-			input.prop('checked', selected);
-		});
-	}
+    function onSearchSettingModalOpened(modal) {
+        var searchKey = modal.data('searchKey');
+        var trigger = $('.search-box-setting[data-key="' + searchKey + '"]');
+        var values = trigger.data('value').split(',');
+        modal.find('input').each(function () {
+            var input = $(this);
+            var selected = values.indexOf(input.val()) !== -1;
+            input.prop('checked', selected);
+        });
+    }
 
-	function onSearchSettingClicked(modal) {
-		setTimeout(function () {
-			var button = modal.find('.btn-primary');
-			var noSelections = (modal.find('input:checked').length === 0);
-			button.prop('disabled', noSelections);
-		}, 50);
-	}
+    function onSearchSettingClicked(modal) {
+        setTimeout(function () {
+            var button = modal.find('.btn-primary');
+            var noSelections = (modal.find('input:checked').length === 0);
+            button.prop('disabled', noSelections);
+        }, 50);
+    }
 
-	function onSearchSettingChanged(modal) {
-		var selections = modal.find('input:checked');
-		if (selections.length === 0) return;
-		var selectionText = joinElements(selections, function (e) { return $(e).next('label').text(); }, ', ');
-		var selectionValue = joinElements(selections, function (e) { return $(e).val(); }, ',');
-		var trigger = getSearchSettingsTrigger(modal);
-		var setting = trigger.siblings('.search-box-setting');
-		var key = setting.data('key');
-		setSearchSetting(key, selectionValue, selectionText);
+    function onSearchSettingChanged(modal) {
+        var selections = modal.find('input:checked');
+        if (selections.length === 0) return;
+        var selectionText = joinElements(selections, function (e) { return $(e).next('label').text(); }, ', ');
+        var selectionValue = joinElements(selections, function (e) { return $(e).val(); }, ',');
+        var trigger = getSearchSettingsTrigger(modal);
+        var setting = trigger.siblings('.search-box-setting');
+        var key = setting.data('key');
+        setSearchSetting(key, selectionValue, selectionText);
 
-		var searchBox = trigger.closest('.search-box');
-		var data = searchBox.data() || {};
-		if (data.settingsCookie) {
-			var settings = getSearchSettings(searchBox);
-			saveSearchSettingsToCookie(data.settingsCookie, settings);
-		}
+        var searchBox = trigger.closest('.search-box');
+        var data = searchBox.data() || {};
+        if (data.settingsCookie) {
+            var settings = getSearchSettings(searchBox);
+            saveSearchSettingsToCookie(data.settingsCookie, settings);
+        }
 
-		if (key === COUNTRY_SETTING_KEY) userHasChangedCountry = true;
-	}
+        if (key === COUNTRY_SETTING_KEY) userHasChangedCountry = true;
+    }
 
-	function setSearchSetting(key, value, text) {
-		var setting = $('.search-box-setting[data-key="' + key + '"]');
-		setting.data('value', value);
-		setting.text(text);
-	}
+    function setSearchSetting(key, value, text) {
+        var setting = $('.search-box-setting[data-key="' + key + '"]');
+        setting.data('value', value);
+        setting.text(text);
+    }
 
-	function getSearchSettingsFromCookie(cookieName) {
-		$.cookie.json = true;
-		return $.cookie(cookieName) || {};
-	}
+    function getSearchSettingsFromCookie(cookieName) {
+        $.cookie.json = true;
+        return $.cookie(cookieName) || {};
+    }
 
-	function saveSearchSettingsToCookie(cookieName, settings) {
-		$.cookie.json = true;
-		$.cookie(cookieName, settings, { path: '/' });
-	}
+    function saveSearchSettingsToCookie(cookieName, settings) {
+        $.cookie.json = true;
+        $.cookie(cookieName, settings, { path: '/' });
+    }
 
-	function getSearchSettingsTrigger(modal) {
-		return $('.search-box-settings [data-target="#' + modal.attr('id') + '"]');
-	}
+    function getSearchSettingsTrigger(modal) {
+        return $('.search-box-settings [data-target="#' + modal.attr('id') + '"]');
+    }
 
-	function onGeolocation(e, args) {
-		if (userHasChangedCountry || !args.countryRegion) return;
+    function onGeolocation(e, args) {
+        if (userHasChangedCountry || !args.countryRegion) return;
 
-		// hack: Using the :contains() selector was causing an internal jQuery exception, so this is a workaround
-		$('.modal[data-search-key="' + COUNTRY_SETTING_KEY + '"] .form-group label').each(function () {
-			var label = $(this);
-			var text = label.text();
-			if (text !== args.countryRegion) return true;
+        // hack: Using the :contains() selector was causing an internal jQuery exception, so this is a workaround
+        $('.modal[data-search-key="' + COUNTRY_SETTING_KEY + '"] .form-group label').each(function () {
+            var label = $(this);
+            var text = label.text();
+            if (text !== args.countryRegion) return true;
 
-			var modal = label.closest('.modal');
-			var trigger = getSearchSettingsTrigger(modal);
-			var searchBox = trigger.closest('.search-box');
-			if (searchBox.data('countryLoaded')) return false;
+            var modal = label.closest('.modal');
+            var trigger = getSearchSettingsTrigger(modal);
+            var searchBox = trigger.closest('.search-box');
+            if (searchBox.data('countryLoaded')) return false;
 
-			var value = label.siblings('input').val();
-			setSearchSetting(COUNTRY_SETTING_KEY, value, text);
-			return false;
-		});
-	}
+            var value = label.siblings('input').val();
+            setSearchSetting(COUNTRY_SETTING_KEY, value, text);
+            return false;
+        });
+    }
 
-	function getSearchBoxElement(el) {
-		return el.is('.search-box') ? el : el.closest('.search-box');
-	}
+    function getSearchBoxElement(el) {
+        return el.is('.search-box') ? el : el.closest('.search-box');
+    }
 
-	function getSearchSettings(searchBox) {
-		var elements = searchBox.find('.search-box-setting');
-		if (elements.length === 0) return {};
-		var settings = {};
-		elements.each(function () {
-			var setting = $(this);
-			settings[setting.data('key')] = { value: setting.data('value'), text: setting.text() };
-		});
-		return settings;
-	}
+    function getSearchSettings(searchBox) {
+        var elements = searchBox.find('.search-box-setting');
+        if (elements.length === 0) return {};
+        var settings = {};
+        elements.each(function () {
+            var setting = $(this);
+            settings[setting.data('key')] = { value: setting.data('value'), text: setting.text() };
+        });
+        return settings;
+    }
 
-	function renderRecentSearches(searchBox) {
-		var container = searchBox.find('.recent-searches');
-		if (container.length === 0) {
-			return;
-		}
+    function renderRecentSearches(searchBox) {
+        var container = searchBox.find('.recent-searches');
+        if (container.length === 0) {
+            return;
+        }
 
-		var cookieName = searchBox.data('cookieName');
-		if (!cookieName) {
-			container.hide();
-			return;
-		}
+        var cookieName = searchBox.data('cookieName');
+        if (!cookieName) {
+            container.hide();
+            return;
+        }
 
-		var searches = $.fn.siteSearch.get(cookieName);
-		if (searches.length === 0) {
-			container.hide();
-			return;
+        var searches = $.fn.siteSearch.get(cookieName);
+        if (searches.length === 0) {
+            container.hide();
+            return;
 
-		}
+        }
 
-		var links = [];
-		for (var i = 0; i < searches.length; i++) {
-			links.push('<a target="_self" href="' + searches[i].Value + '">' + searches[i].Key + '</a>');
-		}
-		container.find('.recent-searches-links').html(links.join(', '));
-		container.show();
-	}
+        var links = [];
+        for (var i = 0; i < searches.length; i++) {
+            links.push('<a target="_self" href="' + searches[i].Value + '">' + searches[i].Key + '</a>');
+        }
+        container.find('.recent-searches-links').html(links.join(', '));
+        container.show();
+    }
 
-	function joinElements(elements, mapFn, separator) {
-		var strings = [];
-		elements.each(function (i, e) { strings.push(mapFn(e)); });
-		return strings.join(separator);
-	}
+    function joinElements(elements, mapFn, separator) {
+        var strings = [];
+        elements.each(function (i, e) { strings.push(mapFn(e)); });
+        return strings.join(separator);
+    }
+
+    $(".hasclear").keyup(function () {
+        var t = $(this);
+        t.next('span').toggle(Boolean(t.val()));
+    });
+
+    $(".clearer").show($(this).prev('input').val());
+
+    $(".clearer").click(function () {
+        $(this).prev('input').val('').focus();
+        $(this).show();
+    });
+      
+
+
+
+
+
+
+
 
 }(window.jQuery));;
 
