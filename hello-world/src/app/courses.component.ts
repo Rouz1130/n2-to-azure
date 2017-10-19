8ddeef0ec389a83ae0100012c6f8a9a954fc82a6
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-10-19 10:44:13 -0700

    add javascript expression for data binding

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>{{title}}</h2>'
+  template: '<h2>{{"Title:" + title}}</h2>'
 })
 
 export class CoursesComponent {
