69fa04b4d6b84d011e06eaabc655f090c43a2587
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-10-19 11:06:28 -0700

    add display a list of courses using ngFor directive fix foo to course

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -6,8 +6,8 @@ import {Component} from '@angular/core';
   template: `
     <h2>{{title}}</h2>
     <ul>
-        <li *ngFor="let foo of courses">
-          {{foo}}
+        <li *ngFor="let course of courses">
+          {{course}}
         </li>
 
     </ul>
