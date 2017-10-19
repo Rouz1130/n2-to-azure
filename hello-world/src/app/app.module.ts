12f3cb0daf0c9125f14c2230a9bfc2ebc48a9756
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-10-19 11:28:40 -0700

    added dependency injection of courses service to allow for loosley coupled service.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 
+import {CoursesService} from './courses.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,9 @@ import { CourseComponent } from './course/course.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -19,8 +19,7 @@ export class CoursesComponent {
   title = "List of courses";
   courses;
 
-  constructor () {
-    let service = new CoursesService();
+  constructor (service: CoursesService) {
     this.courses = service.getCourses();
   }
 }
